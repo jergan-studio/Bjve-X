66d065836154bec55481eed541f6999aae899830
jergan-studio <jose343lopez22@gmail.com>
2026-05-29 10:55:05 -0500

    Update Bjve.core.js

--- Src/Bjve.core.js
+++ Src/Bjve.core.js
@@ -1,1 +1,14 @@
-window.BJVE = {};
+/* =========================
+   BJVE CORE
+========================= */
+
+window.BJVE = window.BJVE || {};
+
+/* =========================
+   INTERNET CONTROL FLAG
+   (White Internet System)
+========================= */
+
+BJVE.internet = {
+  white: false // default = OFF (safe mode)
+};
